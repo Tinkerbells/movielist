92cc45f0298c54d19218956afe0fb5d1a6acdb53
Untermensch <untermenschH@protonmail.com>
2022-11-26 21:06:39 +0300

    add movie list card component

---
+++ src/components/UI/MovieListCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MovieListCard = () => {
+    return (
+        <div className="h-11 w-full rounded-lg bg-white cursor-pointer transition duration-150 ease-in-out hover:bg-gray">
+            MovieListCard
+        </div>
+    )
+}
+
+export default MovieListCard
