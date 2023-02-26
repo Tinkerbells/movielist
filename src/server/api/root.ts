7e8c6317605740af9965c95d6dffc4d63568617a
untermensch <untermenschH@protonmail.com>
2023-02-26 17:23:47 +0300

    add movie router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,7 @@
 import { createTRPCRouter } from "@/server/api/trpc";
 import { exampleRouter } from "@/server/api/routers/example";
 import { tmdbMovieRouter } from "./routers/tmdb";
+import { movieRouter } from "./routers/movie";
 
 /**
  * This is the primary router for your server.
@@ -10,6 +11,7 @@ import { tmdbMovieRouter } from "./routers/tmdb";
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
   tmdb: tmdbMovieRouter,
+  movie: movieRouter,
 });
 
 // export type definition of API
---
+++ src/server/api/routers/movie.ts
@@ -0,0 +1,51 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
+} from "@/server/api/trpc";
+
+export const movieRouter = createTRPCRouter({
+  addFavorite: protectedProcedure
+    .input(
+      z.object({
+        movieId: z.number(),
+        title: z.string(),
+        posterPath: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      return ctx.prisma.favorite.create({
+        data: {
+          movieId: input.movieId,
+          title: input.title,
+          posterPath: input.posterPath,
+        },
+      });
+    }),
+  deleteFavorite: protectedProcedure
+    .input(
+      z.object({
+        movieId: z.number(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      return ctx.prisma.favorite.deleteMany({
+        where: {
+          movieId: input.movieId,
+        },
+      });
+    }),
+  getFavorites: protectedProcedure.query(async ({ ctx }) => {
+    return ctx.prisma.favorite.findMany();
+  }),
+
+  findFavorite: protectedProcedure
+    .input(z.object({ movieId: z.number() }))
+    .query(async ({ ctx, input }) => {
+      return ctx.prisma.favorite.findFirst({
+        where: { movieId: input.movieId },
+      });
+    }),
+});
