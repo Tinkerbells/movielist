52d930e72fa91dda7e0333bc637960aa910714f0
untermensch <untermenschH@protonmail.com>
2023-03-03 18:22:31 +0300

    add country flags to movie pid page

--- src/pages/movie/[pid].tsx
+++ src/pages/movie/[pid].tsx
@@ -1,9 +1,10 @@
 import { IconProvider, Spinner } from "@/UI";
 import { api } from "@/utils/api";
+import { hasFlag } from "country-flag-icons";
 import dayjs from "dayjs";
 import { type NextPage } from "next";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { AiFillStar } from "react-icons/ai";
 
 const MoviePage: NextPage = () => {
@@ -17,10 +18,10 @@ const MoviePage: NextPage = () => {
   }, [pid]);
 
   return (
-    <div className="flex h-full w-full flex-col px-20">
+    <div className="flex h-full w-full justify-center px-80">
       {!isLoading && movie ? (
-        <div className="min-h-screen p-24">
-          <section className="flex gap-10">
+        <div className="mt-24">
+          <div className="flex gap-10">
             <figure>
               {movie.adult ? (
                 <div className="badge-error badge absolute m-3 ">R</div>
@@ -32,15 +33,13 @@ const MoviePage: NextPage = () => {
               />
               <div className="mt-4 flex gap-4">
                 {movie.genres.slice(0, 3).map((genre) => (
-                  <div className="badge-outline badge p-4">{genre.name}</div>
+                  <div className="badge-outline badge p-3">{genre.name}</div>
                 ))}
               </div>
             </figure>
-            <div className="flex flex-col gap-10">
-              <div className="flex items-end gap-32">
-                <h1 className="mt-3 text-4xl font-bold">
-                  {movie.title} ({dayjs(movie.release_date).format("YYYY")})
-                </h1>
+            <section className="flex flex-col gap-5">
+              <h1 className="mt-3 flex items-end gap-10 text-4xl font-bold">
+                {movie.title} ({dayjs(movie.release_date).format("YYYY")})
                 <div className="flex flex-col gap-1">
                   <p className="text-xl text-base-content">TMDB rating</p>
                   <div className="flex items-center gap-1">
@@ -52,13 +51,29 @@ const MoviePage: NextPage = () => {
                     </p>
                   </div>
                 </div>
-              </div>
-              <div>
-                <h2 className="mb-2 text-2xl font-bold">Overview</h2>
-                <p>{movie.overview}</p>
-              </div>
-            </div>
-          </section>
+              </h1>
+              <p className="flex gap-2 text-2xl font-bold">
+                Countries
+                {movie.production_countries.map((country) => (
+                  <figure className="flex items-center gap-2">
+                    {hasFlag(country.iso_3166_1) ? (
+                      <img
+                        alt={country.name}
+                        src={`http://purecatamphetamine.github.io/country-flag-icons/3x2/${country.iso_3166_1}.svg`}
+                        className="max-h-6 rounded-md"
+                      />
+                    ) : null}
+                    <span className="text-lg">{country.iso_3166_1}</span>
+                  </figure>
+                ))}
+              </p>
+              <p>
+                <span className="mb-2 text-2xl font-bold">Overview</span>
+                <br />
+                {movie.overview}
+              </p>
+            </section>
+          </div>
           <div>
             <p className="py-6">
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
