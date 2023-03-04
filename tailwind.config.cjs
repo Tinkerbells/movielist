fe18f726407d81b2b198e1116c06e85cb87e7c30
untermensch <untermenschH@protonmail.com>
2023-03-04 18:46:05 +0300

    add screen sizes to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,12 +4,15 @@ module.exports = {
   theme: {
     extend: {},
     screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
       "3xl": "1920px",
       // => @media (min-width: 1920px) { ... }
       "4xl": "2560px",
       // => @media (min-width: 2560px) { ... }
-      "5xl": "3840px",
-      // => @media (min-width: 3840px) { ... }
     },
   },
   plugins: [require("daisyui")],
